6e27f74d177409a3d683b2d8ab9b7ded2233f6e9
Thailan Swaity <th4lans@gmail.com>
2024-05-27 18:46:05 -0500

    Little cleanup

--- LEDMatrix.c
+++ LEDMatrix.c
@@ -8,17 +8,6 @@
 #define latchPin 2 // ST_CP Pin of 74HC595
 #define clockPin 3 // SH_CP Pin of 74HC595
 
-// column data of the smily face
-unsigned char pic[]={0x1c,0x22,0x51,0x45,0x45,0x51,0x22,0x1c};
-                    //0    0    1    1    1    1    0    0
-                    //0    1    0    0    0    0    1    0
-                    //1    0    0    1    1    0    0    1
-                    //1    0    0    0    0    0    0    1
-                    //1    0    1    0    0    1    0    1
-                    //0    1    0    0    0    0    1    0
-                    //0    0    1    1    1    1    0    0
-                    //0    0    0    0    0    0    0    0
-//
 clock_t last_update_time;
 int cells[COLS][ROWS] = {0};
 int debug_index;
@@ -43,21 +32,6 @@ int main(void) {
 }
 
 void run(void) {
-//  cells[3][0] = cells[3][7] = 1;
-//  cells[4][0] = cells[4][7] = 1;
-//  cells[5][0] = cells[5][7] = 1;
-//
-//  cells[2][1] = cells[2][6] = 1;
-//  cells[6][1] = cells[6][6] = 1;
-//
-//  cells[1][2] = cells[1][5] = 1;
-//  cells[3][2] = cells[3][5] = 1;
-//  cells[7][2] = cells[7][5] = 1;
-//
-//  cells[1][3] = cells[1][4] = 1;
-//  cells[5][3] = cells[5][4] = 1;
-//  cells[7][3] = cells[7][4] = 1;
-  
   cells[6][7] = 1;
   cells[7][6] = 1;
   cells[5][5] = 1;
