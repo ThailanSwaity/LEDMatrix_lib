3fb405f8f98c902ca8a61c0db023d2e2408ccef6
Thailan Swaity <th4lans@gmail.com>
2024-05-26 19:25:33 -0500

    changes

--- LEDMatrix.c
+++ LEDMatrix.c
@@ -17,8 +17,13 @@ unsigned char pic[]={0x1c,0x22,0x51,0x45,0x45,0x51,0x22,0x1c};
                     //0    1    0    0    0    0    1    0
                     //0    0    1    1    1    1    0    0
                     //0    0    0    0    0    0    0    0
+//
+time_t last_update_time;
+int cells[COLS][ROWS] = {0};
+int debug_index;
 
-void init(void);
+void run(void);
+void update(void);
 
 int main(void) {
 
@@ -28,46 +33,49 @@ int main(void) {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
 
-  init();
+  run();
 
   return 0;
 }
 
-void init(void) {
+void run(void) {
+  cells[3][0] = cells[3][7] = 1;
+  cells[4][0] = cells[4][7] = 1;
+  cells[5][0] = cells[5][7] = 1;
 
-  int cells[COLS][ROWS] = {0};
+  cells[2][1] = cells[2][6] = 1;
+  cells[6][1] = cells[6][6] = 1;
 
-//  cells[3][0] = cells[3][7] = 1;
-//  cells[4][0] = cells[4][7] = 1;
-//  cells[5][0] = cells[5][7] = 1;
-//
-//  cells[2][1] = cells[2][6] = 1;
-//  cells[6][1] = cells[6][6] = 1;
-//
-//  cells[1][2] = cells[1][5] = 1;
-//  cells[3][2] = cells[3][5] = 1;
-//  cells[7][2] = cells[7][5] = 1;
-//
-//  cells[1][3] = cells[1][4] = 1;
-//  cells[5][3] = cells[5][4] = 1;
-//  cells[7][3] = cells[7][4] = 1;
+  cells[1][2] = cells[1][5] = 1;
+  cells[3][2] = cells[3][5] = 1;
+  cells[7][2] = cells[7][5] = 1;
 
-  time_t last_update_time = time(0);
-  int index = 0;
+  cells[1][3] = cells[1][4] = 1;
+  cells[5][3] = cells[5][4] = 1;
+  cells[7][3] = cells[7][4] = 1;
+
+  last_update_time = time(0);
+  debug_index = 0;
   
   while(1) {
 
+    // Updates every second
     if (time(0) - last_update_time >= 1) {
-      cells[index % COLS][index / ROWS] = 1;
-       
-      index++;
-      if (index > COLS * ROWS) {
-        index = 0;
-      }
+      update();
+
       last_update_time = time(0);
     }
 
     draw_cells(cells, dataPin, latchPin, clockPin);
   }
 
+}
+
+void update(void) {
+    cells[index % COLS][index / ROWS] = 1;
+      
+    debug_index++;
+    if (debug_index > COLS * ROWS) {
+      debug_index = 0;
+    }
 }
